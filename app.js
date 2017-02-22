e78bf65d76a6923e150eb1cddacd468ae6277a10
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-02-22 20:19:19 +0200

    change to my beloved zeinab

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Zeinab!')
+  res.send('Hello my beloved Zeinab!')
 })
 
 app.listen(8080, function () {
