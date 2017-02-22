b68db6ff28af00e9ea6573097c9a1866372328f0
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-02-22 20:23:17 +0200

    change to farshad

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello my beloved Zeinab!')
+  res.send('Hello Farshad!')
 })
 
 app.listen(8080, function () {
