a87c0d03d109b8c715dc721819d95281395e9d62
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-02-24 11:02:25 +0200

    log

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var app = express()
 var spawn = require('child_process').spawn;
 
 app.get('/', function (req, res) {
-  var child = spawn('ls', ["-l"]);
+  /*var child = spawn('ls', ["-l"]);
   child.stdout.on("data", function(data){
     var str = String(data);
     console.log(str);
     res.send('Hello Farshad!');
-  });
-  //res.send('Hello Farshad!');
+  });*/
+  console.log(__dirname);
+  res.send('Hello Farshad!');
   
 })
 
